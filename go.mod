c85d2af16326b507e162ecd029634bb4efd8706d
Rakshita Varadarajan <rakshitajps@gmail.com>
2020-12-07 20:00:55 +0300

    Changes made to Key Value Store to make it REST Accessible

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rak108/Key-Value-Store
+
+go 1.15
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"sync"
+
+	"github.com/gorilla/mux"
 )
 
 type key_value_store struct {
@@ -12,79 +15,104 @@ type key_value_store struct {
 	mu       sync.RWMutex
 }
 
-func (key_value *key_value_store) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (key_value *key_value_store) getHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	switch r.Method {
-	case "GET":
-		w.WriteHeader(http.StatusOK)
-		key_value.mu.RLock()
-		w.Write([]byte(`{"GET request successful"}`))
-		key_value.mu.RUnlock()
-
-	case "POST":
-		w.WriteHeader(http.StatusCreated)
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprintf(w, "ParseForm() err: %v", err)
-			return
-		}
-		//w.Write([]byte(`{"message": "post called"}`))
-		key_value.mu.Lock()
-		Key := r.FormValue("Key")
-		Value := r.FormValue("Value")
-		if _, check := key_value.database[Key]; !check {
-			key_value.database[Key] = Value
-			w.Write([]byte(`{"POST request successful: Created entry"}`))
-		}
-		key_value.mu.Unlock()
-
-	case "PUT":
-		w.WriteHeader(http.StatusAccepted)
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprintf(w, "ParseForm() err: %v", err)
-			return
-		}
-		key_value.mu.Lock()
-		Key := r.FormValue("Key")
-		Value := r.FormValue("Value")
-		if _, check := key_value.database[Key]; check {
-			key_value.database[Key] = Value
-			w.Write([]byte(`{"PUT request successful: Created entry"}`))
-		}
-		else
-		{
-			w.Write([]byte(`{"Inavlid Key-Value Pair"}`))
-		}
-		key_value.mu.Unlock()
-
-	case "DELETE":
-		w.WriteHeader(http.StatusOK)
-		key_value.mu.Lock()
-		Key := r.URL.Path[8:]
-		//Value := r.FormValue("Value")
-		check:=delete(key_value.database,Key)
-		if check==true{
-			w.Write([]byte(`{"DELETE request successful: Created entry"}`))
-		}
-		else
-		{
-			w.Write([]byte(`{"Inavlid Key-Value Pair"}`))
-		}
-		key_value.mu.Unlock()
-
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(`{"message": "not found"}`))
+	w.WriteHeader(http.StatusOK)
+	key_value.mu.RLock()
+	fmt.Fprintf(w, "GET request accepted:\n")
+	identify := mux.Vars(r)
+	key := identify["key"]
+	fmt.Fprintf(w, "For Key: %s,\n", key)
+	if _, check := key_value.database[key]; check {
+		value := key_value.database[key]
+		fmt.Fprintf(w, "Value: %s\n", value)
+	} else {
+		fmt.Fprintf(w, "Key-Value pair does not exist\n")
 	}
+	key_value.mu.RUnlock()
+}
 
+func (key_value *key_value_store) postHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	key_value.mu.Lock()
+	fmt.Fprintf(w, "POST request accepted:\n")
+	identify := mux.Vars(r)
+	key := identify["key"]
+	value := identify["value"]
+	fmt.Fprintf(w, "For Key: %s,\n", key)
+	fmt.Fprintf(w, "Value: %s\n", value)
+	if _, check := key_value.database[key]; !check {
+		key_value.database[key] = value
+		fmt.Fprintf(w, "Key-Value store updated.\n")
+	} else {
+		fmt.Fprintf(w, "Key-Value pair already exists.\n")
+	}
+	key_value.mu.Unlock()
+}
+
+func (key_value *key_value_store) putHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	key_value.mu.Lock()
+	fmt.Fprintf(w, "PUT request accepted:\n")
+	identify := mux.Vars(r)
+	key := identify["key"]
+	value := identify["value"]
+	fmt.Fprintf(w, "For Key: %s,\n", key)
+	if _, check := key_value.database[key]; check {
+		key_value.database[key] = value
+		fmt.Fprintf(w, "Value: %s\n", value)
+		fmt.Fprintf(w, "Key-Value store updated.\n")
+	} else {
+		fmt.Fprintf(w, "Key-Value pair does not exist.\n")
+	}
+	key_value.mu.Unlock()
+}
+
+func (key_value *key_value_store) delHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	key_value.mu.Lock()
+	fmt.Fprintf(w, "DELETE request accepted:\n")
+	identify := mux.Vars(r)
+	key := identify["key"]
+	fmt.Fprintf(w, "Key: %s\n", key)
+	if _, check := key_value.database[key]; check {
+		delete(key_value.database, key)
+		fmt.Fprintf(w, "Key-Value pair deleted.")
+	} else {
+		fmt.Fprintf(w, "Key-Value pair does not exist.")
+	}
+	key_value.mu.Unlock()
+}
+
+func synerror(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "Incorrect Syntax, nothing found")
+}
+
+func (key_value *key_value_store) getExisting(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "Key-Value pairs:\n")
+	json.NewEncoder(w).Encode(key_value.database)
 }
 
 func main() {
 
-	key_value = &key_value_store{}
-    http.HandleFunc("/",key_value.ServeHTTP)
-	//Start the server and listen for requests
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	key_value := &key_value_store{}
+	key_value.database = make(map[string]string)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/existing", key_value.getExisting).Methods("GET")
+	r.HandleFunc("/{key}", key_value.getHandler).Methods(http.MethodGet)
+	r.HandleFunc("/{key}-{value}", key_value.postHandler).Methods("POST")
+	r.HandleFunc("/{key}-{value}", key_value.putHandler).Methods(http.MethodPut)
+	r.HandleFunc("/{key}", key_value.delHandler).Methods(http.MethodDelete)
+	r.HandleFunc("/", synerror)
+
+	fmt.Printf("Starting server at port 8080...\n")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
